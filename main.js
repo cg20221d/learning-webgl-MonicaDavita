97d71aa8234ad3beeb6669ca8e64e370d5066838
monicadavita <monicadavita19@gmail.com>
2022-09-14 14:52:34 +0700

    adding a red point

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ function main() {
     const gl = kanvas.getContext("webgl");
 
     //vertex shader
-    var vertexShaderCode = 
-    "void main() {" +
-    "}";
+    var vertexShaderCode = `
+    void main() { 
+      float x = 0.0;
+      float y = 0.0;
+      gl_PointSize = 10.0;
+      gl_Position = vec4(x, y, 0.0, 1.0);
+    }
+    `;
 
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject, vertexShaderCode);
@@ -18,7 +23,12 @@ function main() {
     //fragment shader
     var fragmentShaderCode = `
     void main() {
-
+      precision mediump float;
+      float r = 1.0;
+      float g = 0.0;
+      float b = 0.0;
+      float a = 1.0;
+      gl_FragColor = vec4(r, g, b, a);
     }
     `;
 
@@ -32,7 +42,7 @@ function main() {
     gl.attachShader(shaderProgram,fragmentShaderObject);
     gl.linkProgram(shaderProgram);
 
-    gl.useProgram(shaderProgram)
+    gl.useProgram(shaderProgram);
 
     // Only continue if WebGL is available and working
     if (gl === null) {
@@ -41,10 +51,13 @@ function main() {
     }
   
     // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  //dari kiri ke kanan Red --> Green --> Blue --> Transparansi
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  //BLACK
+    //dari kiri ke kanan Red --> Green --> Blue --> Transparansi
 
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);  
+
+    gl.drawArrays(gl.POINTS, 0, 1);
   }
   
   window.onload = main;
