f40077497d87bef44dd33ef704037db362ce5606
monicadavita <monicadavita19@gmail.com>
2022-09-14 15:19:28 +0700

    adding 4 points and making a square

--- main.js
+++ main.js
@@ -6,11 +6,23 @@ function main() {
     // Initialize the GL context
     const gl = kanvas.getContext("webgl");
 
+    var vertices = [
+      0.5, 0.5, //A: kanan atas
+      0.0, 0.0, //B: bawah tengah
+      -0.5, 0.5, // C: kiri atas
+      0.0, 1.0  //D: atas tengah
+    ]; // x1, y1, x2, y2, x3, y3
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     //vertex shader
     var vertexShaderCode = `
+    attribute vec2 aPosition;
     void main() { 
-      float x = 0.0;
-      float y = 0.0;
+      float x = aPosition.x;
+      float y = aPosition.y;
       gl_PointSize = 10.0;
       gl_Position = vec4(x, y, 0.0, 1.0);
     }
@@ -44,6 +56,13 @@ function main() {
 
     gl.useProgram(shaderProgram);
 
+    // Mengajari GPU bagaimana caranya mengoleksi nilai posisi dari ARRAY_BUFFER
+    // Untuk setiap verteks yang sedang diproses
+
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     // Only continue if WebGL is available and working
     if (gl === null) {
       alert("Unable to initialize WebGL. Your browser or machine may not support it.");
@@ -57,7 +76,7 @@ function main() {
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);  
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
   }
   
   window.onload = main;
