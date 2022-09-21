90537eddc16887f8dbe76fa8bdfd58ad996ebd5d
monicadavita <monicadavita19@gmail.com>
2022-09-21 13:45:46 +0700

    making a gradation

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function main() {
     const gl = kanvas.getContext("webgl");
 
     var vertices = [
-      0.5, 0.5, //A: kanan atas
-      0.0, 0.0, //B: bawah tengah
-      -0.5, 0.5, // C: kiri atas
-      0.0, 1.0  //D: atas tengah
+      0.5, 0.5, 0.996, 0.578, 0.578, //A: kanan atas (FF9494 --> 0.996, 0.578, 0.578)
+      0.0, 0.0, 0.996, 0.816, 0.816, //B: bawah tengah (FFD1D1 --> 0.996, 0.816, 0.816)
+      -0.5, 0.5, 0.996, 0.887, 0.879, // C: kiri atas (FFE3E1 --> 0.996, 0.887, 0.879)
+      0.0, 1.0, 0.998, 0.957, 0.891, //D: atas tengah (FFF5E4 --> 0.998, 0.957, 0.891)
     ]; // x1, y1, x2, y2, x3, y3
 
     var buffer = gl.createBuffer();
@@ -20,11 +20,14 @@ function main() {
     //vertex shader
     var vertexShaderCode = `
     attribute vec2 aPosition;
+    attribute vec3 aColor;
+    varying vec3 vColor;
     void main() { 
       float x = aPosition.x;
       float y = aPosition.y;
       gl_PointSize = 10.0;
       gl_Position = vec4(x, y, 0.0, 1.0);
+      vColor = aColor;
     }
     `;
 
@@ -34,13 +37,10 @@ function main() {
 
     //fragment shader
     var fragmentShaderCode = `
+    precision mediump float;
+    varying vec3 vColor;
     void main() {
-      precision mediump float;
-      float r = 1.0;
-      float g = 0.0;
-      float b = 0.0;
-      float a = 1.0;
-      gl_FragColor = vec4(r, g, b, a);
+      gl_FragColor = vec4(vColor, 1.0);
     }
     `;
 
@@ -60,8 +60,12 @@ function main() {
     // Untuk setiap verteks yang sedang diproses
 
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
     gl.enableVertexAttribArray(aPosition);
+
+    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aColor);
 
     // Only continue if WebGL is available and working
     if (gl === null) {
